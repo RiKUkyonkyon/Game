e0c18ae06b47e87b012008751ef0865ab2145794
C20064160 <73428375+C20064160@users.noreply.github.com>
2020-12-20 21:07:03 +0900

    分割コンパイルを実行

--- Game/src/Game.java
+++ Game/src/Game.java
@@ -1,22 +1,17 @@
 import java.util.*;
+import constants.Constants;
 
 class Game{
 	public static void main(String []args){
 
-		//ちんこ
+		System.out.println(constants.Constants.speed_index);
+
 
 		// 定義
         int enemy_size = 5;
-        //enemy_sizeは敵の数を定義した。
-		int parametar_size = 6;
+		int parameter_size = 6;
 		/*パラメータ生成*/
-		int hitpoint=0;
-		int attack=1;
-		int defence=2;
-		int magic_attack=3;
-		int magic_defence=4;
-		int speed = 5;
-
+		
 		//敵の定義
 		int riku=0,kyon=1,hibiki=2,karen=3,nozomi=4;
 
@@ -34,26 +29,31 @@ class Game{
 		/*ここまで導入*/
 	
 		
-		
+		/*
 		int[] parametersUser={hitpoint,attack,defence,magic_attack,magic_defence};
-		
+		*/
 		for(int i=0;i<=4;i++){
 			Random random=new Random();
-			
+			/*
 			parametersUser[i] = random.nextInt(10);
-			
+			*/
 		}
-		System.out.println("\n体力は"  + (parametersUser[hitpoint]+1));
-		System.out.println("攻撃力は"  + (parametersUser[attack]+1));
+		/*
+		System.out.println("\n体力は"  + (parametersUser[hitpoint_index]+1));
+		System.out.println("攻撃力は"  + (parametersUser[attack_index]+1));
 		System.out.println("防御力は"  +( parametersUser[defence]+1));
 		System.out.println("特攻は"  + (parametersUser[magic_attack]+1));
 		System.out.println("特防は"  + (parametersUser[magic_defence]+1));
 		/*終わり*/
 		
 		
-		int enemy[][] = makeArray(enemy_size, parametar_size); //敵の情報を取得
-		printMatrix(enemy, enemy_size, parametar_size); //敵の情報を仮に表示
+		int enemy[][] = makeArray(enemy_size, parameter_size); //敵の情報を取得
+		printMatrix(enemy, enemy_size, parameter_size); //敵の情報を仮に表示
 		
+		System.out.print("\n");
+		
+		int user[][] = makeArray(1,parameter_size);
+		printMatrix(user,1,parameter_size);
 		
 	}
 
@@ -62,16 +62,14 @@ class Game{
 	配列をランダムに初期化するメソッド
 	引数１：行数
 	引数２：列数
-	戻り値：2次正方行列
+	戻り値：int型2次元配列
 	*/
 	static int[][] makeArray(int row,int column){
-
+		int rand_min=30;
+		int rand_max = 50;
 		int[][] enemy=new int[row][column];
 		/*列が敵の名前になります。
 		行がそれぞれのパラメーターになります。*/
-
-        int rand_min=30;
-		int rand_max = 50;
 		for(int i=0;i<row;i++){
 			for(int j=0;j<column;j++){
 				Random randomEnemy=new Random();
---
+++ Game/src/constants/Constants.java
@@ -0,0 +1,15 @@
+package constants;
+
+public class Constants {
+    private Constants(){};
+
+    //定数
+        public static int hitpoint_index=0;
+		public static int attack_index=1;
+		public static int defence_index=2;
+		public static int magic_attack_index=3;
+		public static int magic_defence_index=4;
+		public static int speed_index = 5;
+	
+}
+
